aaa571785ef62a8d5f740913d63817caa4e0ef69
jingisium <jingisium@gmail.com>
2020-10-06 12:44:16 +0900

    新規登録・ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: {with: PASSWORD_REGEX}, length: {minimum: 6}
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
   validates :date_of_birth, presence: true
-  
+
 end
