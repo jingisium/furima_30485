8ac699c5e7d1dd9c1049b312eab3472f7c4d82d9
jingisium <jingisium@gmail.com>
2020-10-07 14:23:35 +0900

    FactoryBot(Item)の定義

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    title                {Faker::Lorem.sentence}
+    description          {Faker::Lorem.sentence}
+    category_id          {"2"}
+    condition_id         {"2"}
+    delivery_charge_id   {"2"}
+    send_from_id         {"2"}
+    days_for_delivery_id {"2"}
+    price                {"300"}
+
+    association          :user
+
+    after(:build) do |content|
+      content.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
