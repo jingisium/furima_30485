f21008c4537f7503379aef65899b623865cb5d73
jingisium <jingisium@gmail.com>
2020-10-07 19:03:34 +0900

    出品者以外をリダイレクトする機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    if user_signed_in? && current_user.id != @item.user.id
+      redirect_to root_path
+    end
   end
 
   private
