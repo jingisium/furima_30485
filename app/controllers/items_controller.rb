24d6f93718d39913c9d99e1d2b6230c64e1de8b0
jingisium <jingisium@gmail.com>
2020-10-07 11:40:33 +0900

    item#newとitem#createの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :category_id,
+                                 :condition_id, :days_for_delivery_id, :delivery_charge_id,
+                                 :send_from_id, :price, :image).merge(user_id: current_user.id)
   end
 end
