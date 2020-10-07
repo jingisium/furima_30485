b802727a62fd66f713e2568cada0cb336e23a762
jingisium <jingisium@gmail.com>
2020-10-07 15:49:41 +0900

    Itemモデルの単体テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -33,25 +33,50 @@ RSpec.describe "商品出品機能", type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
+    it "カテゴリーが「--」を選択されていると保存できない" do
+      @item.category_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
     it "商品の状態が空だと保存できない" do
       @item.condition_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition can't be blank")
+    end
+    it "商品の状態が「--」を選択されていると保存できない" do
+      @item.condition_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition must be other than 1")
     end
     it "配送料の負担が空だと保存できない" do
       @item.delivery_charge_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
     end
+    it "配送料の負担が「--」を選択されていると保存できない" do
+      @item.delivery_charge_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery charge must be other than 1")
+    end
     it "発送元の地域が空だと保存できない" do
       @item.send_from_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Send from can't be blank")
     end
+    it "発送元の地域が「--」を選択されていると保存できない" do
+      @item.send_from_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Send from must be other than 1")
+    end
     it "発送までの日数が空だと保存できない" do
       @item.days_for_delivery_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Days for delivery can't be blank")
+    end
+    it "発送までの日数が「--」を選択されていると保存できない" do
+      @item.days_for_delivery_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Days for delivery must be other than 1")
     end
     it "価格が空だと保存できない" do
       @item.price = nil
