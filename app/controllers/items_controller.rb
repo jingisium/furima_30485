adf97d98066183322010259feb6290310fcbdba0
jingisium <jingisium@gmail.com>
2020-10-08 11:26:23 +0900

    削除ボタンを押すとデータベースから削除されてトップページに戻る

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,6 +37,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    end
   end
 
   private
