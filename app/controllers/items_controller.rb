7562dd0a58ffeb1bb9e8bd0e367f0482a1ecb9f0
jingisium <jingisium@gmail.com>
2020-10-08 11:31:04 +0900

    destroyアクションのコード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,9 +38,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if item.destroy
-      redirect_to root_path
-    end
+    item.destroy
+    redirect_to root_path
   end
 
   private
