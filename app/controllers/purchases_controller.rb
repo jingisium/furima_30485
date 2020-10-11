9bb8827fdb62bbbfacd5cac478eba7e786c4ff42
jingisium <jingisium@gmail.com>
2020-10-11 17:02:53 +0900

    ログアウトユーザーのログイン画面誘導

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,6 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @item = Item.find(params[:item_id])
     @purchase_buyer = PurchaseBuyer.new
