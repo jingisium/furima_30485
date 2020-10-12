c23ca202db7f38f8995ec61192e4a52f11d54ca2
jingisium <jingisium@gmail.com>
2020-10-12 14:13:00 +0900

    purchasesコントローラー内のメソッド切り分け

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,8 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only:[:index, :create]
   before_action :authenticate_user!
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_buyer = PurchaseBuyer.new
     if (user_signed_in? && current_user.id == @item.user_id) || @item.sold == true
       redirect_to root_path
@@ -10,7 +10,6 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_buyer = PurchaseBuyer.new(purchase_params)
     if @purchase_buyer.valid?
       pay_item
@@ -23,6 +22,10 @@ class PurchasesController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def purchase_params
     params.require(:purchase_buyer).permit(:post_code, :prefecture_id, :city, :street, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
