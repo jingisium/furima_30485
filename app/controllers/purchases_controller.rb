d3d4161bba480f218bfdfd81eb2c3f83973dcc6a
jingisium <jingisium@gmail.com>
2020-10-12 11:08:30 +0900

    リダイレクト機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,6 +4,9 @@ class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @purchase_buyer = PurchaseBuyer.new
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def create
